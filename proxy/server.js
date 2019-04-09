28fbdf6ce553f08ce5c63a0196592c7ebd2fb7f2
Gary <garyguan0713@gmail.com>
2019-04-09 15:26:56 -0700

    Merge earning rating, price average on proxy

---
+++ proxy/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
