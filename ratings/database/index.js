76ca59c3d8a9088813b4a97a1f324f58c607467b
Gary <garyguan0713@gmail.com>
2019-04-11 21:15:48 -0700

    Database seeding

--- ratings/database/index.js
+++ ratings/database/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const Rating = require('./AnalystRating/AnalystRatingScheme.js');
 
+const mongoUri = 'mongodb://172.17.0.5/stock';
 // const mongoUri = 'mongodb://localhost/stock';
-const mongoUri = 'mongodb://gary:abcd1234@ds031922.mlab.com:31922/front-end-capstone-project';
+// const mongoUri = 'mongodb://gary:abcd1234@ds031922.mlab.com:31922/front-end-capstone-project';
 // const mongoUri = process.env.DATABASEURL;
 mongoose.connect(mongoUri, { useNewUrlParser: true },
   (err) => {
