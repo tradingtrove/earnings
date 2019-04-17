050286a6ef62576056bafb224e71b77d3a6ae7d5
Peter R <pryzanych@gmail.com>
2019-04-17 16:57:35 -0700

    Create first draft of DataGen algorithm. 100 companies at 1.369 seconds.

---
+++ earnings/database/Earning/earningsDataGen.js
@@ -0,0 +1,63 @@
+const faker = require('faker');
+
+
+
+const companyData = [];
+const tickers = [];
+const allQuarters = ['Q4 2017', 'Q1 2018', 'Q2 2018', 'Q3 2018', 'Q4 2018', 'Q1 2019', 'Q2 2019'];
+allEarnings = [];
+
+let createTicker = () => {
+  let ticker = '';
+  let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+  for (let i = 0; i < 5; i++) {
+    ticker += alphabet[Math.floor(Math.random() * 26)]
+  }
+  if (tickers.includes(ticker)) {
+    createTicker();
+  } else {
+    tickers.push(ticker);
+  }
+}
+
+console.time();
+for (let i = 0; i < 1000; i++) {
+  companyData.push({
+    id: i.toString(),
+    ticker: createTicker(),
+    company: faker.company.companyName(),
+  });
+}
+console.timeEnd();
+
+console.time();
+for (const company of companyData) {
+  let actualEarning = Math.random() * 7;
+  let estimatedEarning;
+  let quarterNumber = 0;
+  for (const quarter of allQuarters) {
+    let range = Math.floor(Math.random() * 100);
+    range *= Math.floor(Math.random() * 2) === 1 ? 0.45 : -0.40;
+    actualEarning *= (1 + range / 100);
+    actualEarning = actualEarning.toFixed(2);
+
+    let estimateRange = Math.floor(Math.random() * 100);
+    estimateRange *= Math.floor(Math.random() * 2) === 1 ? 0.10 : -0.10;
+    estimatedEarning = actualEarning * (1 + estimateRange / 100);
+    estimatedEarning = estimatedEarning.toFixed(2);
+
+    allEarnings.push({
+      id: company.id,
+      company: company.company,
+      quarter,
+      quarterNumber,
+      actualEarning: Number(actualEarning),
+      estimatedEarning: Number(estimatedEarning),
+    });
+    quarterNumber += 1;
+  }
+}
+console.timeEnd()
+
+console.log(allEarnings);
