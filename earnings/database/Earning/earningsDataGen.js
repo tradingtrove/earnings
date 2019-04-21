2a905a8511228fe000bd22fb0023797f09ca709d
Peter R <pryzanych@gmail.com>
2019-04-20 17:55:28 -0700

    Refactor DataGen to not hit heap limit

--- earnings/database/Earning/earningsDataGen.js
+++ earnings/database/Earning/earningsDataGen.js
@@ -3,75 +3,71 @@ const fs = require('fs');
 
 const file = fs.createWriteStream('./DataGen.csv');
 
-const tickers = {};
-let allEarnings = [];
 const allQuarters = ['Q4 2017', 'Q1 2018', 'Q2 2018', 'Q3 2018', 'Q4 2018', 'Q1 2019', 'Q2 2019'];
-
-let createTicker = () => {
-  let ticker = '';
-  let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-  for (let i = 0; i < 5; i++) {
-    ticker += alphabet[Math.floor(Math.random() * 26)]
-  }
-  if (tickers[ticker]) {
-    createTicker();
-  } else {
-    tickers[ticker] = ticker;
-  }
-  return ticker;
-}
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 let generation = () => {
-  const company = {
-    ticker: createTicker(),
-    company: faker.company.companyName().replace(/[,]/, ''),
-  }
-  let companyEarnings = [];
-  let actualEarning = Math.random() * 7;
+  // let allEarnings = [];
+  // let companyData = [];
+  let companyEarnings;
+  let actualEarning;
   let estimatedEarning;
-  let quarterNumber = 0;
-  for (const quarter of allQuarters) {
-    let range = Math.floor(Math.random() * 100);
-    range *= Math.floor(Math.random() * 2) === 1 ? 0.45 : -0.40;
-    actualEarning *= (1 + range / 100);
-    actualEarning = actualEarning.toFixed(2);
+  let quarterNumber;
+  let range;
+  let estimateRange;
+  ((length, limit) => {
+    alphabet = alphabet.split('');
+    let counter = 0;
+    let result;
+    let recurse = (string) => {
+      if (counter === limit) {
+        return;
+      }
+      if (string.length === length) {
+        counter++;
+        const company = {
+          ticker: string,
+          company: faker.company.companyName().replace(/[,]/, ''),
+        }
+        
+        companyEarnings = [];
+        actualEarning = Math.random() * 7;
+        quarterNumber = 0;
+        for (const quarter of allQuarters) {
+          range = Math.floor(Math.random() * 100);
+          range *= Math.floor(Math.random() * 2) === 1 ? 0.45 : -0.40;
+          actualEarning *= (1 + range / 100);
+          actualEarning = actualEarning.toFixed(2);
 
-    let estimateRange = Math.floor(Math.random() * 100);
-    estimateRange *= Math.floor(Math.random() * 2) === 1 ? 0.10 : -0.10;
-    estimatedEarning = actualEarning * (1 + estimateRange / 100);
-    estimatedEarning = estimatedEarning.toFixed(2);
+          estimateRange = Math.floor(Math.random() * 100);
+          estimateRange *= Math.floor(Math.random() * 2) === 1 ? 0.10 : -0.10;
+          estimatedEarning = actualEarning * (1 + estimateRange / 100);
+          estimatedEarning = estimatedEarning.toFixed(2);
 
-    companyEarnings.push({
-      ticker: company.ticker,
-      company: company.company,
-      quarter,
-      quarterNumber,
-      actualEarning: Number(actualEarning),
-      estimatedEarning: Number(estimatedEarning),
-    });
-    quarterNumber += 1;
-  }
-  allEarnings.push(companyEarnings);
-}
-
-let convertToCsv = (array) => {
-  let csvString = '';
-  array.forEach( (company) => {
-    for (let i = 0; i < company.length; i++) {
-      csvString += Object.values(company[i]).join() + '\n';
+          companyEarnings.push(`${company.ticker},${company.company},${quarter},${quarterNumber},${actualEarning},${estimatedEarning}\n`);
+          quarterNumber += 1;
+        }
+        result = companyEarnings.join('');
+        writeOneMillionTimes(file, result);
+        return;
+      }
+      alphabet.forEach( (letter) => {
+        recurse(string + letter);
+      });
     }
-  });
-  return csvString;
+    recurse('');
+  })(5, 10000000);
 }
 
-let writeOneMillionTimes = (writer, encoding, callback) => {
-  let i = 2;
+
+let writeOneMillionTimes = (writer, data, encoding, callback) => {
+  let i = 1;
   write = () => {
+    if (i % 100 === 0) {
+      console.error(i * 1000);
+    }
     let ok = true;
     do {
-      generation();
-      let data = convertToCsv(allEarnings);
       i--;
       if (i === 0) {
         writer.write(data, encoding, callback);
@@ -87,5 +83,5 @@ let writeOneMillionTimes = (writer, encoding, callback) => {
   write();
 }
 
-writeOneMillionTimes(file);
+generation();
  
