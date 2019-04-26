3623680c95103be613559e7b377e122ede29719d
Peter R <pryzanych@gmail.com>
2019-04-26 15:15:12 -0700

    Refactor proxy server to only run earnings module

--- proxy/server.js
+++ proxy/server.js
@@ -1,3 +1,4 @@
+const nr = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -6,7 +7,7 @@ const path = require('path');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
